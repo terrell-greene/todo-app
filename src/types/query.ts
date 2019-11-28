01d43a519a9a79b8ea5a4a7be8b1ae96f056e914
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2019-11-27 22:26:00 -0600

    Added sorting on user queries, and added a task query

--- src/types/query.ts
+++ src/types/query.ts
@@ -2,12 +2,36 @@ import { queryType } from 'nexus'
 
 export const Query = queryType({
   definition(t) {
-    t.string('hello', { resolve: () => 'Hello from the api' })
+    t.field('user', {
+      type: 'User',
+      resolve: async (_, args, { db, request }) => {
+        const {
+          body: { userId }
+        } = request
+
+        const user = await db.User.findById(userId).populate({
+          path: 'tasks',
+          options: { sort: { rank: 'asc' } }
+        })
+
+        return user!
+      }
+    })
 
     t.list.field('users', {
       type: 'User',
-      resolve: (_, args, ctx) => {
-        return ctx.db.User.find()
+      resolve: async (_, args, ctx) => {
+        return ctx.db.User.find().populate({
+          path: 'tasks',
+          options: { sort: { rank: 'asc' } }
+        })
+      }
+    })
+
+    t.list.field('tasks', {
+      type: 'Task',
+      resolve: async (_, args, ctx) => {
+        return ctx.db.Task.find()
       }
     })
   }
