b63a769f4d7808a5cc14e875cb1be8a6fdb704af
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2019-11-27 22:23:44 -0600

    added an async for each method to the utils file

--- src/utils.ts
+++ src/utils.ts
@@ -8,3 +8,12 @@ export const isEmpty = (value: any) =>
   value === undefined ||
   value === null ||
   (typeof value === 'object' && Object.keys(value).length === 0)
+
+export async function asyncForEach<T>(
+  array: Array<T>,
+  callback: (value: T, index: number, array: Array<T>) => void
+) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array)
+  }
+}
