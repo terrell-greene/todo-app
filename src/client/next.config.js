6a03a5f4dd212c5cafe8a5d2ae297a6932099b6e
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2019-11-28 09:45:42 -0600

    Added sass to client project

---
+++ src/client/next.config.js
@@ -0,0 +1,11 @@
+const withSass = require("@zeit/next-sass");
+const withCSS = require("@zeit/next-css");
+
+module.exports = withCSS(
+  withSass({
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: "[local]___[hash:base64:5]"
+    }
+  })
+);
