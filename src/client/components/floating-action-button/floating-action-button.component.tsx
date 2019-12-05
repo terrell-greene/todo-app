35acb7667845f30cd7c851f98c0d19b05f830fa6
terrell-greene <33211911+terrell-greene@users.noreply.github.com>
2019-12-04 19:32:14 -0600

    Updated styling for action button

--- src/client/components/floating-action-button/floating-action-button.component.tsx
+++ src/client/components/floating-action-button/floating-action-button.component.tsx
@@ -43,7 +43,7 @@ const FloatingActionButton: React.FC = () => {
     <div className="floating-action-button">
       <Popover
         content={hoverContent}
-        trigger="hover"
+        trigger="click"
         visible={hovered}
         onVisibleChange={() => setHovered(!hovered)}
       >
